a1a8f9b058877a255865525ec36268ab0ef8fe22
unknown <rektpunk@gmail.com>
2025-04-16 18:23:22 +0900

    change datetime for news, change refine logic somebit

--- refine.py
+++ refine.py
@@ -2,27 +2,36 @@ import pandas as pd
 
 
 def refine_data(data: pd.DataFrame) -> pd.DataFrame:
-    data.loc[data["is_posted"] == 0]  # FIXME: 물결님 헤엎-!
-    
+    _data = data.loc[data["is_posted"] == 0]
+    ## TODO: 현재 시점에 따라 시점을 변경하는 코드 작성
+    ## 예, 20250416 이니까 -> 15일전을 기준으로 20250401이라는 형 구하기
+    ## np.issubdtype(_data['post_date'], np.int | np.float)에 따라서 숫자형, 아니면 문자형으로 반환하여 date_criteria 로 할당
+
+    data_criteria = 20250401
+    pattern = "먹통|오류|에러|안됨|짜증|하나은행|하나카드|사용하시는|사용하는"  ## 요거는 조금 위험할 수도 있겠는데요? 논의해봐요~
     # 블로그 refine
-    data_blog = data[data['source'] == 'blog']
-    data_blog = data_blog[~data_blog['description'].str.contains('도용', na=False)]
-    data_blog = data_blog[data_blog['description'].str.contains('먹통|오류|에러|안됨|짜증|하나은행|하나카드|사용하시는|사용하는', na=False)]
-    data_blog = data_blog[data_blog['postdate'] >= 20250300]
+    data_blog = _data.loc[_data["source"] == "blog"]
+    data_blog = data_blog.loc[
+        (~data_blog["description"].str.contains("도용", na=False))
+        & (data_blog["description"].str.contains(pattern, na=False))
+        & (data_blog["post_date"] >= data_criteria)
+    ]
 
     # 카페 refine
-    data_cafe = data[data['source'] == 'cafe']
-    data_cafe = data_cafe[~data_cafe['cafename'].str.contains('베이직피플', na=False)]
-    mask = (
-    data_cafe['description'].str.contains('먹통|오류|에러|안됨|짜증|하나은행|하나카드|사용하시는|사용하는', na=False) |
-    data_cafe['title'].str.contains('먹통|오류|에러|안됨|짜증|하나은행|하나카드|사용하시는|사용하는', na=False)
-    )
-    data_cafe = data_cafe[mask]
+    data_cafe = _data.loc[_data["source"] == "cafe"]
+    data_cafe = data_cafe.loc[
+        (data_cafe["description"].str.contains(pattern, na=False))
+        | (data_cafe["title"].str.contains(pattern, na=False))
+    ]
 
     # 뉴스 refine
-    data_news = data[data['source'] == 'news']
-    data_news['postdate_dt'] = pd.to_datetime(data_news['postdate'], format='%a, %d %b %Y %H:%M:%S %z')
-    data_news = data_news[data_news['postdate_dt'].dt.year == 2025]
-
+    data_news = _data[_data["source"] == "news"]
+    data_news = data_news[data_news["post_date"] >= data_criteria]
 
     return pd.concat([data_blog, data_news, data_cafe], ignore_index=True)
+
+
+if __name__ == "__main__":
+    data = pd.read_csv("data/data.csv", encoding="utf-8")
+    refined_data = refine_data(data)
+    print(refined_data.head())
--- select_column.py
+++ select_column.py
@@ -2,32 +2,31 @@ from typing import Literal
 
 import pandas as pd
 
-SCHEMA: list[str] = [
+COMMON_COLUMNS: list[str] = [
     "title",
     "link",
     "description",
-    "post_date",
     "source",
     "is_posted",
 ]
 
 
 def _select_blog_data(data: pd.DataFrame) -> pd.DataFrame:
-    return data[
-        ["title", "link", "description", "postdate", "source", "is_posted"]
-    ].rename({"postdate": "post_date"}, axis=1)[SCHEMA]
+    return data[COMMON_COLUMNS + ["postdate"]].rename({"postdate": "post_date"}, axis=1)
 
 
 def _select_news_data(data: pd.DataFrame) -> pd.DataFrame:
-    return data[
-        ["title", "link", "description", "pubDate", "source", "is_posted"]
-    ].rename({"pubDate": "post_date"}, axis=1)[SCHEMA]
+    _data = data[COMMON_COLUMNS + ["pubDate"]].rename({"pubDate": "post_date"}, axis=1)
+    _data = _data.assign(
+        post_date=pd.to_datetime(
+            _data["post_date"], format="%a, %d %b %Y %H:%M:%S %z"
+        ).dt.strftime("%Y%m%d"),
+    )
+    return _data
 
 
 def _select_cafe_data(data: pd.DataFrame) -> pd.DataFrame:
-    return data[["title", "link", "description", "source", "is_posted"]].assign(
-        post_date="",
-    )[SCHEMA]
+    return data[COMMON_COLUMNS].assign(post_date="")
 
 
 SOURCES_SELECT_MAP: Literal["blog", "news", "cafe"] = {
