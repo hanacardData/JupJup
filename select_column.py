137d4fe4256dc8b0ebedc0a351bc50920f3e90ca
김물결 <ccomo345@gmail.com>
2025-04-18 13:32:12 +0900

    select_column.py 수정한거 되돌리기..

--- select_column.py
+++ select_column.py
@@ -2,35 +2,29 @@ from typing import Literal
 
 import pandas as pd
 
-COMMON_COLUMNS: list[str] = [
+SCHEMA: list[str] = [
     "title",
     "link",
     "description",
+    "post_date",
     "source",
     "is_posted",
 ]
 
 
 def _select_blog_data(data: pd.DataFrame) -> pd.DataFrame:
-    return data[COMMON_COLUMNS + ["postdate"]].rename({"postdate": "post_date"}, axis=1)
-
-
-def _select_news_data(data: pd.DataFrame) -> pd.DataFrame:
-    _data = data[COMMON_COLUMNS + ["pubDate"]].rename({"pubDate": "post_date"}, axis=1)
-    _data = _data.assign(
-        post_date=pd.to_datetime(
-            _data["post_date"], format="%a, %d %b %Y %H:%M:%S %z"
-        ).dt.strftime("%Y%m%d"),
-    )
-    return _data
+    return data[
+        ["title", "link", "description", "postdate", "source", "is_posted"]
+    ].rename({"postdate": "post_date"}, axis=1)[SCHEMA]
 
 
 def _select_cafe_data(data: pd.DataFrame) -> pd.DataFrame:
-    return data[COMMON_COLUMNS].assign(post_date="")
+    return data[["title", "link", "description", "source", "is_posted"]].assign(
+        post_date="",
+    )[SCHEMA]
 
 
 SOURCES_SELECT_MAP: Literal["blog", "news", "cafe"] = {
     "blog": _select_blog_data,
-    "news": _select_news_data,
     "cafe": _select_cafe_data,
 }
