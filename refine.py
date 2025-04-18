9cabd82a64fb525a8f0b809e4dbf10c0efe43c18
김물결 <ccomo345@gmail.com>
2025-04-18 10:31:45 +0900

    add: date_criteria calculation code

--- refine.py
+++ refine.py
@@ -1,20 +1,20 @@
 import pandas as pd
+from datetime import datetime, timedelta
 
 
 def refine_data(data: pd.DataFrame) -> pd.DataFrame:
     _data = data.loc[data["is_posted"] == 0]
-    ## TODO: 현재 시점에 따라 시점을 변경하는 코드 작성
-    ## 예, 20250416 이니까 -> 15일전을 기준으로 20250401이라는 형 구하기
-    ## np.issubdtype(_data['post_date'], np.int | np.float)에 따라서 숫자형, 아니면 문자형으로 반환하여 date_criteria 로 할당
 
-    data_criteria = 20250401
+    today = datetime.today()
+    date_criteria = today - timedelta(days=15)
+    date_criteria = int(date_criteria.strftime("%Y%m%d"))
     pattern = "먹통|오류|에러|안됨|짜증|하나은행|하나카드|사용하시는|사용하는"  ## 요거는 조금 위험할 수도 있겠는데요? 논의해봐요~
     # 블로그 refine
     data_blog = _data.loc[_data["source"] == "blog"]
     data_blog = data_blog.loc[
         (~data_blog["description"].str.contains("도용", na=False))
         & (data_blog["description"].str.contains(pattern, na=False))
-        & (data_blog["post_date"] >= data_criteria)
+        & (data_blog["post_date"] >= date_criteria)
     ]
 
     # 카페 refine
@@ -24,11 +24,7 @@ def refine_data(data: pd.DataFrame) -> pd.DataFrame:
         | (data_cafe["title"].str.contains(pattern, na=False))
     ]
 
-    # 뉴스 refine
-    data_news = _data[_data["source"] == "news"]
-    data_news = data_news[data_news["post_date"] >= data_criteria]
-
-    return pd.concat([data_blog, data_news, data_cafe], ignore_index=True)
+    return pd.concat([data_blog, data_cafe], ignore_index=True)
 
 
 if __name__ == "__main__":
