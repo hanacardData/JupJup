e209c6a36d2703cd2a51e3242ef90391c6c8beea
RektPunk <rektpunk@gmail.com>
2025-04-21 09:20:36 +0900

    set post message

--- post_message_bot.py
+++ post_message_bot.py
@@ -1,14 +1,19 @@
 import requests
 
 from access_token import token_manager
+from choose import select_post
 
 
-bot_id = 9881957
-channel_id = "8895b3b4-1cff-cec7-b7bc-a6df449d3638"
-token = token_manager.get_token()
-message_payload = {
-    "content": {"type": "text", "text": "안녕하세요! 물결 봇 테스트입니다."}
-}
-url = f"https://www.worksapis.com/v1.0/bots/{bot_id}/channels/{channel_id}/messages"
-headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
-response = requests.post(url, headers=headers, json=message_payload)
+def post_message() -> None:
+    bot_id = 9881957 # 봇 ID; 고정
+    channel_ids: list[str] = ["8895b3b4-1cff-cec7-b7bc-a6df449d3638"] # 채널 ID; 추가할것
+    token = token_manager.get_token()
+    message = select_post()
+    message_payload = {
+        "content": {"type": "text", "text": message} # FIXME: message 형식 변경
+    }
+
+    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+    for channel_id in channel_ids:
+        url = f"https://www.worksapis.com/v1.0/bots/{bot_id}/channels/{channel_id}/messages"
+        requests.post(url, headers=headers, json=message_payload)
