e0d83d1e08eef34b0a080e00527a2d666b603a87
RektPunk <rektpunk@gmail.com>
2025-04-22 16:58:16 +0900

    set main branch to execute

--- run_batch.bat
+++ run_batch.bat
@@ -1,5 +1,7 @@
 @echo off
 cd /d "C:\Users\user\Desktop\main\trend_analysis"
+git checkout main
+git pull origin main
 echo [%date% %time%] Start >> log.txt
 "C:\Users\user\AppData\Local\Programs\Python\Python312\python.exe" batch_runner.py >> log.txt 2>&1
 echo [%date% %time%] Finished >> log.txt
