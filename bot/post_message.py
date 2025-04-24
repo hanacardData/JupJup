1e6d18b1e07ad2abb6088e967efbd90affd9b0c1
unknown <rektpunk@gmail.com>
2025-04-24 18:17:02 +0900

    set channel ids

--- bot/post_message.py
+++ bot/post_message.py
@@ -8,8 +8,8 @@ from bot.message import get_message
 def post_message(data: pd.DataFrame) -> None:
     bot_id = 9881957  # 봇 ID; 고정
     channel_ids: list[str] = [
-        "8895b3b4-1cff-cec7-b7bc-a6df449d3638",
-        # "bf209668-eca1-250c-88e6-bb224bf9071a"
+        "8895b3b4-1cff-cec7-b7bc-a6df449d3638",  # for test
+        "bf209668-eca1-250c-88e6-bb224bf9071a",
     ]  # 채널 ID; 추가할것
     message = get_message(data)
     message_payload = {
