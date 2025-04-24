c371177e8eda27ea235ea3d5988ed65ee94f1bf9
unknown <rektpunk@gmail.com>
2025-04-24 18:10:01 +0900

    change prompt

--- batch_runner.py
+++ batch_runner.py
@@ -1,15 +1,13 @@
 import pandas as pd
 
 from bot.post_message import post_message
-from data_collect.keywords import QUERIES
-from data_collect.load import collect_load_data
 from variables import DATA_PATH
 
 
 def run_all():
     print("Batch Start")
 
-    collect_load_data(QUERIES)
+    # collect_load_data(QUERIES)
     print("Collection Completed.")
 
     df = pd.read_csv(DATA_PATH, encoding="utf-8")
