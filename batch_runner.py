a596c184c631687dcda2d33a7ad9cbb97638ebe4
김물결 <ccomo345@gmail.com>
2025-04-24 18:25:37 +0900

    [Hotfix] change batch runner enable collect data

--- batch_runner.py
+++ batch_runner.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
 from bot.post_message import post_message
+from data_collect.load import collect_load_data
+from data_collect.keywords import QUERIES
 from variables import DATA_PATH
 
 
 def run_all():
     print("Batch Start")
 
-    # collect_load_data(QUERIES)
+    collect_load_data(QUERIES)
     print("Collection Completed.")
 
     df = pd.read_csv(DATA_PATH, encoding="utf-8")
