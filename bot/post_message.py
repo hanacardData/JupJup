a6f69b437de7fc68c43db7fd4907a2160ea5a84a
RektPunk <110188257+RektPunk@users.noreply.github.com>
2025-04-25 18:06:41 +0900

    [Feature] update prompt and split test env (#22)
    * change prompt to make happy captain
    
    * log error to works and split channel test and non test
    
    * add channels

--- bot/post_message.py
+++ bot/post_message.py
@@ -5,22 +5,45 @@ from bot.access_token import token_manager
 from bot.message import get_message
 
 
-def post_message(data: pd.DataFrame) -> None:
+def post_message(data: pd.DataFrame, is_test: bool = False) -> None:
     bot_id = 9881957  # 봇 ID; 고정
+    test_channel_id = "8895b3b4-1cff-cec7-b7bc-a6df449d3638"  # 테스트용 채널 ID
+    base_url = (
+        "https://www.worksapis.com/v1.0/bots/{bot_id}/channels/{channel_id}/messages"
+    )
     channel_ids: list[str] = [
-        "8895b3b4-1cff-cec7-b7bc-a6df449d3638",  # for test
         "bf209668-eca1-250c-88e6-bb224bf9071a",
+        "bb16f67c-327d-68e3-2e03-4215e67f8eb2",
     ]  # 채널 ID; 추가할것
-    message = get_message(data)
-    message_payload = {
-        "content": {
-            "type": "text",
-            "text": message,
-        }
-    }
-
     token = token_manager.get_token()
     headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
-    for channel_id in channel_ids:
-        url = f"https://www.worksapis.com/v1.0/bots/{bot_id}/channels/{channel_id}/messages"
-        requests.post(url, headers=headers, json=message_payload)
+
+    try:
+        message = get_message(data)
+        message_payload = {
+            "content": {
+                "type": "text",
+                "text": message,
+            }
+        }
+        if is_test:
+            url = base_url.format(bot_id=bot_id, channel_id=test_channel_id)
+            requests.post(url, headers=headers, json=message_payload)
+            return
+
+        for channel_id in channel_ids:
+            url = base_url.format(bot_id=bot_id, channel_id=channel_id)
+            requests.post(url, headers=headers, json=message_payload)
+
+    except Exception as e:
+        error_message_payload = {
+            "content": {
+                "type": "text",
+                "text": str(e),
+            }
+        }
+        requests.post(
+            base_url.format(bot_id=bot_id, channel_id=test_channel_id),
+            headers=headers,
+            json=error_message_payload,
+        )
