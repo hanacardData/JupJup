25d5fb1a5ec5bee50179a33f19205f08b7dc1c97
unknown <rektpunk@gmail.com>
2025-04-28 12:25:32 +0900

    change filter logic

--- bot/menu.py
+++ bot/menu.py
@@ -7,8 +7,8 @@ async def select_random_menu(target: str = "점심") -> dict[str, str]:
     if target not in menu_df.columns:
         target = "점심"
     return (
-        menu_df.loc[menu_df[target] != ""]
-        .groupby("구분", group_keys=False)
+        menu_df.loc[~menu_df[target].isna()]
+        .groupby("구분")
         .apply(lambda x: x.sample(1))
         .sample(3)
         .reset_index(drop=True)
