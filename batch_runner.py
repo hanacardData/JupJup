8a0342301129e027ee4cb0dfe7601e65fe15978a
RektPunk <110188257+RektPunk@users.noreply.github.com>
2025-05-02 12:03:22 +0900

    [Feature] 줍줍이의 주5일 근무제를 보장하라! 공휴일을 보장하라! (#32)
    * add holiday skip logic
    
    * add weekday

--- batch_runner.py
+++ batch_runner.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 import pandas as pd
+from holidayskr import is_holiday
 
 from bot.issue import post_issue_message
 from data_collect.keywords import QUERIES
@@ -7,15 +10,24 @@ from logger import logger
 from variables import DATA_PATH
 
 
+def is_weekend(date: datetime.date) -> bool:
+    return date.weekday() >= 5
+
+
 def run_all():
-    logger.info("Batch Start")
+    datetime_now = datetime.now()
+    logger.info(f"Batch Start: {datetime_now}")
 
     collect_load_data(QUERIES)
-    logger.info("Collection Completed.")
+    logger.info(f"Collection Completed: {datetime_now}")
+
+    if is_holiday(datetime_now.strftime("%Y-%m-%d")) or is_weekend(datetime_now):
+        logger.info(f"Not today: {datetime_now}")
+        return
 
     df = pd.read_csv(DATA_PATH, encoding="utf-8")
     post_issue_message(data=df, is_test=False)  # test 시에는 True로 변경
-    logger.info("Sent Message")
+    logger.info(f"Sent Message: {datetime_now}")
 
 
 if __name__ == "__main__":
