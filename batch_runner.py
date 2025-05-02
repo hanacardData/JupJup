eaf144f0fd0a535b82a33f0bb4ff2db288b893c4
unknown <rektpunk@gmail.com>
2025-05-02 11:30:47 +0900

    add weekday

--- batch_runner.py
+++ batch_runner.py
@@ -10,20 +10,24 @@ from logger import logger
 from variables import DATA_PATH
 
 
+def is_weekend(date: datetime.date) -> bool:
+    return date.weekday() >= 5
+
+
 def run_all():
-    datetime_now = datetime.now().strftime("%Y-%m-%d")
+    datetime_now = datetime.now()
     logger.info(f"Batch Start: {datetime_now}")
 
     collect_load_data(QUERIES)
     logger.info(f"Collection Completed: {datetime_now}")
 
-    if is_holiday(datetime_now):
-        logger.info(f"Today is a holiday: {datetime_now}")
+    if is_holiday(datetime_now.strftime("%Y-%m-%d")) or is_weekend(datetime_now):
+        logger.info(f"Not today: {datetime_now}")
         return
 
     df = pd.read_csv(DATA_PATH, encoding="utf-8")
     post_issue_message(data=df, is_test=False)  # test 시에는 True로 변경
-    logger.info("Sent Message")
+    logger.info(f"Sent Message: {datetime_now}")
 
 
 if __name__ == "__main__":
