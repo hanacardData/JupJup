502e1289fc667daec0c15d661bd2a7ebaa942567
RektPunk <110188257+RektPunk@users.noreply.github.com>
2025-05-12 11:16:22 +0900

    [Refactor] move variables into data_collect (#41)
    * remove bot id const
    
    * remove variables
    
    * dont forget issue
    
    * move issue
    
    * remove post message function

--- batch_runner.py
+++ batch_runner.py
@@ -3,18 +3,19 @@ from datetime import datetime
 import pandas as pd
 from holidayskr import is_holiday
 
-from bot.services.issue.issue import post_issue_message
+from bot.services.core.post_message import post_message_to_channel
+from data_collect.issue.make_message import get_issue_message
 from data_collect.keywords import QUERIES
 from data_collect.load import collect_load_data
+from data_collect.variables import DATA_PATH, SUBSCRIBE_CHANNEL_IDS, TEST_CHANNEL_ID
 from logger import logger
-from variables import DATA_PATH
 
 
 def is_weekend(date: datetime.date) -> bool:
     return date.weekday() >= 5
 
 
-def run_all():
+def run_all(is_test: bool = False):
     datetime_now = datetime.now()
     logger.info(f"Batch Start: {datetime_now}")
 
@@ -26,9 +27,20 @@ def run_all():
         return
 
     df = pd.read_csv(DATA_PATH, encoding="utf-8")
-    post_issue_message(data=df, is_test=False)  # test 시에는 True로 변경
+    try:
+        message = get_issue_message(df)
+        logger.info(f"Message ready: {message}")
+        if is_test:
+            post_message_to_channel(message, TEST_CHANNEL_ID)
+            return
+        for channel_id in SUBSCRIBE_CHANNEL_IDS:
+            post_message_to_channel(message, channel_id)
+
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        post_message_to_channel(str(e), TEST_CHANNEL_ID)
     logger.info(f"Sent Message: {datetime_now}")
 
 
 if __name__ == "__main__":
-    run_all()
+    run_all(is_test=False)  # 테스트 시엔 True
