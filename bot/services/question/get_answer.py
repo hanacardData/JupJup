f1c1b822d14eb323c4a46e36a2b91527556ebda8
unknown <rektpunk@gmail.com>
2025-05-14 11:51:10 +0900

    fix question recieve dataset

--- bot/services/question/get_answer.py
+++ bot/services/question/get_answer.py
@@ -1,8 +1,39 @@
+import json
+from datetime import datetime
+
+import pandas as pd
+from fastapi_cache.decorator import cache
+
 from bot.services.core.openai_client import async_openai_response
+from bot.services.question.prompt import PROMPT
+from data_collect.issue.make_message import extract_high_score_data
+from data_collect.variables import DATA_PATH
+
+
+async def get_data() -> pd.DataFrame:
+    return pd.read_csv(DATA_PATH)
+
+
+@cache(expire=43_200)
+async def get_prompt_content() -> str:
+    data = await get_data()
+    refined_data = extract_high_score_data(data, extracted_data_count=10)
+    content = json.dumps(
+        refined_data[["title", "link", "description"]]
+        .astype(str)
+        .to_dict(orient="records"),
+        ensure_ascii=False,
+    )
+    return (
+        f"데이터 수집 날짜: {datetime.today().strftime('%Y년 %m월 %d일')}\n"
+        + f"수집 데이터 수: {len(data)}\n"
+        + f"수집 데이터 예시: {content}\n"
+    )
 
 
 async def get_answer(input: str) -> str:
+    content = await get_prompt_content()
     return await async_openai_response(
-        prompt="당신은 줍줍이라는 하나카드 회사의 챗봇입니다. 질문에 대한 답변을 간결하고 위트있게 존댓말로 답변합니다.",
+        prompt=PROMPT.format(content=content),
         input=input,
     )
