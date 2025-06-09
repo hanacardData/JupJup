5fe9b395806399050f913e5bcce46810edd8c8a2
unknown <rektpunk@gmail.com>
2025-06-09 18:09:40 +0900

    update keyword

--- batch/travellog/keywords.py
+++ batch/travellog/keywords.py
@@ -8,18 +8,7 @@ TRAVELLOG_KEYWORDS: list[str] = [
     "하나머니",
 ]
 
-TRAVELLOG_ISSUE_KEYWORDS: list[str] = [
-    "불편",
-    "오류",
-    "불만",
-    "안돼요",
-    "안되요",
-    "문의",
-    "도와주세요",
-    "절대",
-    "거절",
-    "불안",
-    "급해요",
+TRAVELLOG_ADDTIONAL_KEYWORDS: list[str] = [
     "인출",
     "ATM",
     "출금",
@@ -27,8 +16,28 @@ TRAVELLOG_ISSUE_KEYWORDS: list[str] = [
     "수수료",
 ]
 
-combinations = list(itertools.product(TRAVELLOG_KEYWORDS, TRAVELLOG_ISSUE_KEYWORDS))
+TRAVELLOG_ISSUE_KEYWORDS: list[str] = [
+    "",
+    "불편",
+    "오류",
+    "불가",
+    "안돼요",
+    "안되요",
+    "불만",
+    "문의",
+    "도와주세요",
+    "절대",
+    "거절",
+    "불안",
+    "급해요",
+]
+
+combinations = list(
+    itertools.product(
+        TRAVELLOG_KEYWORDS, TRAVELLOG_ADDTIONAL_KEYWORDS, TRAVELLOG_ISSUE_KEYWORDS
+    )
+)
 TRAVELLOG_QUERIES: list[str] = [
-    re.sub(r"\s+", " ", f"{combination[0]} {combination[1]}").strip()
+    re.sub(r"\s+", " ", f"{combination[0]} {combination[1]} {combination[2]}").strip()
     for combination in combinations
 ]
