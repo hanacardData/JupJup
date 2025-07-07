1b0667a2d488b4a378e069036b7899c100919ca9
unknown <rektpunk@gmail.com>
2025-07-07 17:10:25 +0900

    add comment for bose wave

--- batch/variables.py
+++ batch/variables.py
@@ -9,12 +9,13 @@ TRAVELLOG_DATA_PATH: str = os.path.join(SAVE_PATH, "travellog_data.csv")
 COMPARE_TRAVEL_DATA_PATH: str = os.path.join(SAVE_PATH, "compare_travel_data.csv")
 TEST_CHANNEL_ID: str = "8895b3b4-1cff-cec7-b7bc-a6df449d3638"
 
-# 트래블로그UX부 우수현
+# 트래블로그 블로그글 송신, 트래블로그UX부 우수현
 TRAVELLOG_CHANNEL_ID: str = "59fc2740-d582-8072-4c63-add08f331dda"
 
-# 트래블로그 비교 채널
+# 트래블카드 비교분석, 쏠트래블, 트래블러스, 트래블월렛, 위비트래블 등
 COMPARE_TRAVEL_CHANNEL_ID: str = "5d12d31b-baa8-b92f-ad6b-1e0ef2642e5d"
 
+# 하나카드 이슈
 SUBSCRIBE_CHANNEL_IDS: list[str] = [
     "bf209668-eca1-250c-88e6-bb224bf9071a",  # 데이터 사업부
     "51d15802-cfb6-2e1b-6eb2-c545d2331783",  # CRM 마케팅팀 송치성
