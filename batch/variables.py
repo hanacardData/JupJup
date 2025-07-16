46a0eeaa9b1b98241b430d89195c66217b15d8e3
unknown <rektpunk@gmail.com>
2025-07-16 09:59:46 +0900

    remove channel

--- batch/variables.py
+++ batch/variables.py
@@ -19,7 +19,6 @@ COMPARE_TRAVEL_CHANNEL_IDS: list[str] = [
 SUBSCRIBE_CHANNEL_IDS: list[str] = [
     "51d15802-cfb6-2e1b-6eb2-c545d2331783",  # CRM 마케팅팀 송치성
     "6a7aa514-55a4-0afd-fd3e-4ed14ec6d81e",  # 트래블로그부 허준회
-    "0a7211c1-ae81-e159-55de-7c6d84041508",  # 트래블로그부 허준회
     "749a747c-d3e2-8c28-fc0a-df870e4bfaf5",  # BIZ 운영팀 오지열
     "6a81b6d1-66e2-4da1-d1e0-ccae13d6e26a",  # 직원행복팀 하두선
     "ab53861e-8b62-b64a-7708-5755f1c57259",  # 대구센터 곽승열
