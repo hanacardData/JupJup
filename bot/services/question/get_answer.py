3b0590cfe77ee3a462673d481b2b7c9dcc2e3a01
김물결 <ccomo345@gmail.com>
2025-07-18 17:53:33 +0900

    수집한 데이터 저장해서 불러오는 로직 삭제

--- bot/services/question/get_answer.py
+++ bot/services/question/get_answer.py
@@ -1,45 +1,9 @@
-import json
-from datetime import datetime
-
-import pandas as pd
-from fastapi_cache.decorator import cache
-
-from batch.issue.keywords import CARD_PRODUCTS, ISSUE_KEYWORDS
-from batch.issue.make_message import extract_high_score_data
-from batch.variables import DATA_PATH, EXTRACTED_DATA_COUNT
 from bot.services.core.openai_client import async_openai_response
 from bot.services.question.prompt import PROMPT
 
 
-@cache(expire=43_200)
-async def get_prompt_content() -> str:
-    data = pd.read_csv(DATA_PATH, dtype={"post_date": object})
-    refined_data = extract_high_score_data(
-        data=data,
-        issue_keywords=ISSUE_KEYWORDS,
-        product_keywords=CARD_PRODUCTS,
-        extracted_data_count=EXTRACTED_DATA_COUNT,
-    )
-    content = json.dumps(
-        refined_data[["title", "link", "description"]]
-        .astype(str)
-        .to_dict(orient="records"),
-        ensure_ascii=False,
-    )
-    return (
-        f"데이터 수집 날짜: {datetime.today().strftime('%Y년 %m월 %d일')}\n"
-        + f"현재까지 수집한 총 데이터 수: {len(data)}\n"
-        + f"소개할 집중 분석 대상 데이터 수: {EXTRACTED_DATA_COUNT}\n"
-        + f"소개할 집중 분석 데이터: {content}\n"
-    )
-
-
 async def get_answer_comment(input: str) -> str:
-    content = await get_prompt_content()
     return await async_openai_response(
-        prompt=PROMPT.format(
-            content=content,
-            card_products=", ".join(CARD_PRODUCTS),
-        ),
+        prompt=PROMPT,
         input=input,
     )
