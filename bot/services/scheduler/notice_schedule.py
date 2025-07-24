25b9089689fb1026b8b2c99c1054f37a443f00d9
unknown <rektpunk@gmail.com>
2025-07-24 10:06:19 +0900

    revert schedule

--- bot/services/scheduler/notice_schedule.py
+++ bot/services/scheduler/notice_schedule.py
@@ -1,17 +1,10 @@
-import os
 from datetime import datetime
 
-# from apscheduler.jobstores.json import JSONJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from bot.services.core.post_message import post_message_to_channel
 
-jobstore_dir = "jobs"
-jobstore_file = "jobs.json"
-os.makedirs(jobstore_dir, exist_ok=True)
-
 scheduler = BackgroundScheduler()
-# scheduler.add_jobstore(JSONJobStore(path=os.path.join(jobstore_dir, jobstore_file)))
 scheduler.start()
 
 
