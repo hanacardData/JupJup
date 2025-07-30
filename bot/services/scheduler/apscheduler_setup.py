64ed67fc266afb2b005d65539204939264bdce27
김물결 <ccomo345@gmail.com>
2025-07-30 16:15:46 +0900

    refactor: add debuging check code

--- bot/services/scheduler/apscheduler_setup.py
+++ bot/services/scheduler/apscheduler_setup.py
@@ -1,3 +1,4 @@
+import redis
 from apscheduler.jobstores.redis import RedisJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
@@ -13,3 +14,6 @@ scheduler = AsyncIOScheduler(
     },
     timezone="Asia/Seoul",
 )
+
+r = redis.Redis(host="127.0.0.1", port=6379, db=0, decode_responses=True)
+r.set("whoami_bot", "i_am_fastapi_server")
