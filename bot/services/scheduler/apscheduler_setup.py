85a7ba66c8ab51f81ac8f8eab745c491a468dff6
unknown <rektpunk@gmail.com>
2025-07-30 17:08:55 +0900

    [Hotfix] revert redis host

--- bot/services/scheduler/apscheduler_setup.py
+++ bot/services/scheduler/apscheduler_setup.py
@@ -6,7 +6,7 @@ scheduler = AsyncIOScheduler(
         "default": RedisJobStore(
             jobs_key="apscheduler.jobs",
             run_times_key="apscheduler.run_times",
-            host="redis",
+            host="127.0.0.1",
             port=6379,
             db=0,
         )
