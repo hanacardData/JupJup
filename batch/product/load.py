6a389fb947beece9bbd0d6b955a883d28bf89ec5
unknown <rektpunk@gmail.com>
2025-11-11 17:36:21 +0900

    refactor product

--- batch/product/load.py
+++ batch/product/load.py
@@ -1,27 +1,40 @@
 import os
 from datetime import datetime
 from time import sleep
+from typing import Literal
 
 import pandas as pd
 from tqdm import tqdm
 
 from batch.fetch import fetch_data
+from batch.product.keywords import (
+    CREDIT_CARD_KEYWORDS,
+    DEBIT_CARD_KEYWORDS,
+    JADE_CARD_FEEDBACK_KEYWORDS,
+    WONDER_CARD_FEEDBACK_KEYWORDS,
+)
 from batch.product.select_column import SOURCES_SELECT_MAP
 from batch.utils import read_csv
+from batch.variables import PRODUCT_SAVE_PATH
 from logger import logger
+
+FILE_TAG_QUERIES_MAP = {
+    "credit": CREDIT_CARD_KEYWORDS,
+    "debit": DEBIT_CARD_KEYWORDS,
+    "wonder": JADE_CARD_FEEDBACK_KEYWORDS,
+    "jade": WONDER_CARD_FEEDBACK_KEYWORDS,
+}
 
 
 def load_competitor_issues(
-    queries: list[str],
-    save_path: str,
-    file_tag: str,
+    file_tag: Literal["credit", "debit"],
 ) -> None:
     """경쟁사 신상품: 뉴스만 수집"""
-    os.makedirs(save_path, exist_ok=True)
-    file_path = os.path.join(save_path, f"news_{file_tag}.csv")
+    os.makedirs(PRODUCT_SAVE_PATH, exist_ok=True)
+    file_path = os.path.join(PRODUCT_SAVE_PATH, f"news_{file_tag}.csv")
     existing_data = read_csv(file_path)
     items: list[dict[str, str]] = []
-    for keyword in tqdm(queries, desc="news", leave=False):
+    for keyword in tqdm(FILE_TAG_QUERIES_MAP[file_tag], desc="news", leave=False):
         result = fetch_data("news", keyword, display=100, sort="sim")
         sleep(0.1)
         if result is None:
@@ -50,28 +63,28 @@ def load_competitor_issues(
 
     df.to_csv(file_path, index=False, encoding="utf-8")
     SOURCES_SELECT_MAP["news"](df).to_csv(
-        os.path.join(save_path, f"{file_tag}.csv"), index=False, encoding="utf-8"
+        os.path.join(PRODUCT_SAVE_PATH, f"{file_tag}.csv"),
+        index=False,
+        encoding="utf-8",
     )
     logger.info(f"{file_path} scrap completed")
 
 
 def load_ourproduct_issues(
-    queries: list[str],
-    save_path: str,
-    file_tag: str,
+    file_tag: Literal["wonder", "jade"],
 ) -> None:
     """자사 원더/JADE: 뉴스+블로그 수집"""
-    os.makedirs(save_path, exist_ok=True)
-    file_name = os.path.join(save_path, f"{file_tag}.csv")
+    os.makedirs(PRODUCT_SAVE_PATH, exist_ok=True)
+    file_name = os.path.join(PRODUCT_SAVE_PATH, f"{file_tag}.csv")
     _df_list: list[pd.DataFrame] = [read_csv(file_name)]
 
     for source in ["news", "blog"]:
-        file_path = os.path.join(save_path, f"{source}_{file_tag}.csv")
+        file_path = os.path.join(PRODUCT_SAVE_PATH, f"{source}_{file_tag}.csv")
 
         existing_data = read_csv(file_path)
         items: list[dict[str, str]] = []
 
-        for keyword in tqdm(queries, desc=source, leave=False):
+        for keyword in tqdm(FILE_TAG_QUERIES_MAP[file_tag], desc=source, leave=False):
             result = fetch_data(source, keyword, display=100, sort="sim")
             sleep(0.1)
             if result is None:
