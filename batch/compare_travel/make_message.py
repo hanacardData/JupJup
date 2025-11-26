83ac56824bdea1bcc4823c56951de7156c36009f
unknown <rektpunk@gmail.com>
2025-11-26 15:09:12 +0900

    remove openai response in compare travel py

--- batch/compare_travel/make_message.py
+++ batch/compare_travel/make_message.py
@@ -5,10 +5,10 @@ from batch.compare_travel.keywords import (
 )
 from batch.compare_travel.prompt import PROMPT, TEXT_INPUT
 from batch.fetch import fetch_trend_data
-from bot.services.core.openai_client import openai_response
+from bot.services.core.openai_client import async_openai_response
 
 
-def _get_trend_message():
+async def _get_trend_message():
     today = datetime.today().strftime("%Y-%m-%d")
     one_week_ago = (datetime.today() - timedelta(days=7)).strftime("%Y-%m-%d")
     trend_response = fetch_trend_data(
@@ -17,15 +17,16 @@ def _get_trend_message():
         timeUnit="date",
         keywordGroups=COMPARE_TRAVEL_TREND_KEYWORDS,
     )
-    return openai_response(
+    return await async_openai_response(
         prompt=PROMPT,
         input=TEXT_INPUT.format(content=trend_response.to_results()),
     )
 
 
-def get_compare_travel_message() -> list[str]:
+async def get_compare_travel_message() -> list[str]:
+    trend_message = await _get_trend_message()
     message = [
         "트래블카드의 최근 7일 간 네이버 검색어 트렌드 분석 결과를 확인해보세요! 📊\n",
-        _get_trend_message(),
+        trend_message,
     ]
     return message
