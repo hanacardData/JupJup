93926025f76752023c89e29be04089d3d85084ae
RektPunk <rektpunk@gmail.com>
2025-12-03 10:06:21 +0900

    [Feature] create pr action (#177)

--- secret.py
+++ secret.py
@@ -2,14 +2,22 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
+if os.path.exists(".env"):
+    load_dotenv()
+
 
 CLIENT_ID = os.environ["client_id"]
 CLIENT_SECRET = os.environ["client_secret"]
-OPENAI_API_KEY = os.environ["openai_api_key"]
+OPENAI_API_KEY = os.environ.get("openai_api_key")
 WORKS_CLIENT_ID = os.environ["works_client_id"]
 WORKS_CLIENT_SECRET = os.environ["works_client_secret"]
 SERVICE_ACCOUNT = os.environ["service_account"]
-PRIVATE_KEY_PATH = os.environ["private_key_path"]
+
+_PRIVATE_KEY_PATH = os.environ["private_key_path"]
+if _PRIVATE_KEY_PATH.endswith(".key"):
+    with open(_PRIVATE_KEY_PATH, "r") as key_file:
+        PRIVATE_KEY_PATH = key_file.read()
+else:
+    PRIVATE_KEY_PATH = _PRIVATE_KEY_PATH
+
 BOT_SECRET = os.environ["bot_secret"]
-OPENWEATHER_API_KEY = os.environ["openweather_api_key"]
