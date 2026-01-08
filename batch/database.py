1976bc0cbb63fe87372d11f15a6b2099510de509
unknown <rektpunk@gmail.com>
2026-01-08 12:21:04 +0900

    add init database

---
+++ batch/database.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+
+def init_database():
+    with sqlite3.connect("jupjup.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS geek_news (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                url TEXT UNIQUE NOT NULL,
+                scrapped_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        """)
+        conn.commit()
