102e871090cc4b76e69199950efa69aad9d0fab9
RektPunk <rektpunk@gmail.com>
2026-02-14 13:46:09 +0900

    remove join

--- bot/handler/event.py
+++ bot/handler/event.py
@@ -2,10 +2,15 @@ from fastapi.responses import JSONResponse
 
 from bot.enums.default_messages import Message
 from bot.enums.status import BotStatus
-from bot.handler.channel_message_event import handle_message_event
-from bot.handler.join_event import handle_join_event
+from bot.handler.message.channel import handle_message_event
 from bot.services.core.post_payload import async_post_message
 from logger import logger
+
+
+async def handle_join_event(channel_id: str) -> JSONResponse:
+    """채널에 봇이 추가되었을 때나 도움요청에 호출되는 핸들러입니다."""
+    await async_post_message(Message.GREETINGS_REPLY.value, channel_id)
+    return JSONResponse(status_code=200, content={"status": BotStatus.GREETED})
 
 
 async def process_event(data: dict) -> JSONResponse:
